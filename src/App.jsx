959725acc0af43d19ec63770da3355d2a09d6565
Shubh <shubhjhawar78@gmail.com>
2023-09-03 14:50:47 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white underline font-bold p-4">
+      Hello, Tailwind CSS!
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include JavaScript and TypeScript files
+    './index.html', // Include your HTML file (if you have classes defined there)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
