aae6c4b5bf17db23550b90d0c95d100a39786335
Shubh <shubhjhawar78@gmail.com>
2023-09-04 13:59:33 -0400

    post api for updating started working

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require("dotenv").config();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const connectDb = require('./dbConnection');
 
 connectDb();
@@ -8,8 +9,11 @@ const app = express();
 
 const port = 5000;
 
+// middlewares
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+
 app.use('/api', require("./routes"))
 
 app.listen(port, ()=> {
