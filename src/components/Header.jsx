cd164782aa0365ae8e1aaedb71d0b909601ef8bc
Shubh <shubhjhawar78@gmail.com>
2023-09-03 23:40:27 -0400

    prelim body implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from '../assets/logo-color.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="bg-blue-400 p-8 lg:px-20 md:px-10">
       <div className="flex flex-1 justify-between">
-      <div className='flex inline-flex gap-4 items-center'>
+      <Link to="/" className='flex inline-flex gap-4 items-center'>
         <div>
           <img
             src={logo}
@@ -16,7 +17,7 @@ const Header = () => {
         <div className='max-md:hidden font-semibold text-2xl px-1'>
           JobTrackr
         </div>
-      </div>
+      </Link>
 
       <div className='flex flex-col justify-center'>
         <p className="hidden lg:block font-semibold text-4xl uppercase bg-[#A7F3D0] rounded-xl px-4 py-1 shadow-lg ">We have Got This!</p>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import LeftNav from "./LeftNav.jsx";
+import Jobs from './Jobs.jsx';
+import Home from './Home.jsx';
 
-const Layout = ({children}) => {
+const Layout = () => {
   return (
-    <div className='grid grid-cols-8 min-h-screen'>
+    <div className='grid grid-cols-8  min-h-screen'>
       <div className="col-span-8 ">
         <Header />
       </div>
 
       <div className='lg:col-span-2 md:col-span-1 max-sm:hidden block'>
-        <LeftNav />
+          <Routes>
+            <Route path="/" element={<LeftNav />} />
+            <Route path='/:user/*' element={<LeftNav />} />
+          </Routes>
       </div>
 
-      <div className='col-span-6  sm:h-4/5' >
-        {children}
+      <div className='col-span-8 lg:col-span-6 md:col-span-7 sm:col-span-7 xs:col-span-5' >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/:user' element={<Jobs />} />
+          </Routes>
       </div>
 
       <div className="col-span-8 max-sm:block hidden mt-auto">
-        <Footer />
+          <Routes>
+            <Route path="/" element={<Footer />} />
+            <Route path='/:user/*' element={<Footer />} />
+          </Routes>
       </div>
     </div>
   )
